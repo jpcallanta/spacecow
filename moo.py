d775283dbcb7e44a232933717474bc516b820606
John Callanta <john@cnevids.com>
2015-08-02 11:57:39 -0400

    Initial commit

---
+++ moo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import pyglet
+from pyglet import clock
+from lib.player import Player
+from lib.window import Window
+
+if __name__ == '__main__':
+  window = Window(1024, 768, True)
+  
+  pyglet.clock.schedule_interval(window.update, (1/60.))
+  pyglet.app.run()
